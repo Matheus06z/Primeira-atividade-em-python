e2ee33e8afd98c308a01f78a08cb6a33db073ff3
Matheus06z <matheus.gabriel24091@gmail.com>
2023-03-15 22:08:49 -0300

    Primeira atividade em python

---
+++ main.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome completo: ")
+
+altura = float(input("Digite sua altura: "))
+
+cpf = int(input("Digite seu CPF: "))
+
+print("Seu nome é: ", nome,", altura é:", altura, "e o seu CPF é:", cpf)
+
+str(input("Você confirma isso? "))
+
+print("Ok, Obrigado!")
